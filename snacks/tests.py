1b858378113c882be217acea3627ad92b5cfd30b
AmerAomar <ameralamriii87@gmail.com>
2023-06-11 17:37:26 +0000

    django snacks done with test and without the tailwind styling

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    # Test that home and about url status codes are 200
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+    
+    # Test home and about url template use, including ancestor template.
+    def test_home_page_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_about_page_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertTemplateUsed(response, 'base.html')
